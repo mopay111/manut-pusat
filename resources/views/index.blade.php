348d96ceead63c248b75a47f9a5cc90503535375
mopay111 <danangsetyaadhitama@gmail.com>
2024-04-02 21:02:19 -0700

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+@vite('resources/css/app.css')
+<title>Danang Portfolio</title>
+</head>
+<body class="bg-primary">
+<div class="-mb-16">
+<h1 class="text-center text-8xl text-transparent bg-gradient-to-r from-blue-700 to-white bg-clip-text">Danang Setya Adhitama</h1>
+</div>
+<nav class="flex w-full mx-auto justify-around text-center">
+<img src="https://i.ibb.co/BN21XTP/WIN-20220926-22-57-31-Pro-removebg-preview-1.png" alt="" class="w-[369px] h-[208px]">
+<div class="mt-20 space-x-10 text-3xl bg-gradient-to-r from-red-700 to-white text-transparent bg-clip-text flex flex-wrap">
+<a href="" class="hover:text-white">Home</a>
+<a href="" class="hover:text-white">services</a>
+<a href="" class="hover:text-white">portfolio</a>
+<a href="" class="hover:text-white">about me</a>
+<div class="bg-red-700 w-[510px] h-[20px] -mt-10"></div>
+</div>
+<img src="https://i.ibb.co/j5zzQ6S/WIN-20220926-22-55-39-Pro-removebg-preview-2.png" alt="" class="w-[305px] h-[257px] -mt-10">
+</nav>
+
+<section class="text-center text-4xl justify-center mt-28">
+    <div class="bg-gradient-to-r from-blue-600 to-white text-transparent bg-clip-text">
+        <p class="pb-5">Saya seorang mahasiswa semester 4 prodi teknik komputer</p>
+        <p class="pb-5">dari politeknik harapan bersama tegal ,saya memilih keahlian</p>
+        <p class="pb-5">salah satunya adalah desain dan sebagi programming pemula</p>
+    </div>
+
+    <div class="ml-20 mt-10 flex flex-wrap w-[1110px] text-center justify-center">
+
+        <div class="ml-5 flex">
+            <div  class="ml-20">
+                <img src="https://i.ibb.co/0jVNdHr/th-removebg-preview-1-1.png" alt="" width="320px">
+            </div>
+            <div class="text-yellow-400 flex flex-wrap">
+                <div class="ml-20 mr-40 mb-5">
+                    <h2>Networking</h2>
+                    <div class="bg-gradient-to-r from-blue-700 via-slate-600 to-white w-[300px] mt-3 rounded-full text-center text-base">45%</div>
+                </div>
+
+                <div>
+                    <h2>Programming</h2>
+                    <div class="bg-gradient-to-r from-blue-700 via-slate-600 to-white w-[300px] mt-3 rounded-full text-base">55%</div>
+                </div>
+
+                <div class="ml-20 mr-40">
+                    <h2>Desain</h2>
+                    <div class="bg-gradient-to-r from-blue-700 via-slate-600 to-white w-[300px] mt-3 rounded-full text-center text-base">65%</div>
+                </div>
+
+                <div>
+                    <h2>Office</h2>
+                    <div class="bg-gradient-to-r from-blue-700 via-slate-600 to-white w-[300px] mt-3 rounded-full text-base">40%</div>
+                </div>
+            </div>
+    </div>
+</section>
+
+<section class="text-center text-4xl justify-center mt-40">
+    <div>
+        <h1 class="text-4xl text-white">CONTACT PERSON</h1>
+    </div>
+
+    <div class="mt-10 space-y-5">
+        <div class="ml-48 flex flex-wrap">
+            <img src="https://i.ibb.co/z7WSzW8/download-removebg-preview-1.png" alt="" class="w-10">
+            <h3 class="text-white ml-10 text-5xl">+62 819 0605 1437</h3>
+        </div>
+
+        <div class="ml-48 flex flex-wrap">
+            <img src="https://i.ibb.co/n6YNpxW/OIP-1-removebg-preview-1.png" alt="" class="w-10">
+            <h3 class="text-white ml-10  text-5xl">danangsetyaadhitama@gmail.com</h3>
+        </div>
+
+        <div class="ml-48 flex flex-wrap">
+            <img src="https://i.ibb.co/sjBhjZg/th-2-removebg-preview-1.png" alt="" class="w-10">
+            <h3 class="text-white ml-10  text-5xl">dnngssaa</h3>
+        </div>
+    </div>
+    <footer class="text-white text-4xl mt-12">Membangun kenangan yang tak terlupakan.</footer>
+</section>
+</body>
+</html>
